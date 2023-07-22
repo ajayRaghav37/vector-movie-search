a5488b33ecdf6e3fb144f9bd204214cacc65e668
Ajay RAGHAV <ajay.raghav@mongodb.com>
2023-07-22 20:36:24 +0530

    disable vector search if API key is not added

--- src/App.js
+++ src/App.js
@@ -131,7 +131,7 @@ const MovieApp = () => {
           placeholder="Enter plot here..."
         />
         <div className="dropdown">
-          <button className="dropdown-button submit-button" disabled={searchQuery.trim() === ''} type="submit">
+          <button className="dropdown-button submit-button" disabled={searchQuery.trim() === '' || (selectedOption === "Vector" && apiKey.trim() === '')} type="submit">
             {selectedOption === "Standard" ? "Standard Search" : "Vector Search"}
           </button>
           <button className="dropdown-button arrow" onClick={handleDropdownToggle}>▼</button>
